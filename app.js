8248cc156df5728a224f9a6bf9b42d0f262d313f
Vinicius Holanda <holanda796@gmail.com>
2020-05-19 22:06:14 -0300

    Inserido Date

--- app.js
+++ app.js
@@ -6,37 +6,8 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     var today = new Date();
-    var currentDay = today.getDay()
-    var day = "";
-
-
-    switch (today, currentDay) {
-        case 0:
-           day = "Sunday"
-            break;
-
-        case 1:
-            day ="Monday"
-            break;
-        case 2:
-            day ="thusday"
-            break;
-        case 3:
-            day = "Wedsday"
-            break
-        case 1:
-            day = "Thirsday"
-            break;
-        case 1:
-            day = "Friday"
-            break;
-        case 1:
-            day =  "Saturday"
-            break;
-
-        default:
-            break;
-    }
+    var options = {weekday: "long", month: "long", day: "numeric"}
+    var day = today.toDateString('en-US', options)
 
     res.render("list", {
         kindOfDay: day
