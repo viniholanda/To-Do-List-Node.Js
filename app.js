26d06e9e32cf9168146e413062ace91b442b58a2
Vinicius Holanda <holanda796@gmail.com>
2020-05-22 17:54:36 -0300

    add For

--- app.js
+++ app.js
@@ -3,26 +3,37 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+var itens = [];
+
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     var today = new Date();
-    var options = {weekday: "long", month: "long", day: "numeric"}
+
+    var options = {
+    weekday: "long",
+    month: "long", 
+    day: "numeric"}
+
     var day = today.toDateString('en-US', options)
 
-    res.render("list", {
-        kindOfDay: day
-    })
+    res.render("list", {kindOfDay: day, newListItens: itens})
+
+    
 
 })
 
 app.post("/", function(req, res) {
 
-   var item = req.body.newItem
+ item = req.body.newItem
 
-    console.log(item);
+ itens.push(item)
+
+    res.redirect("/")
+
+   
     
 
 })
